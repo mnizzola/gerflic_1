8b3bb4a2c4f3ae0271679e0a77592286594263ae
mnizzola <maximonizzola@gmail.com>
2018-04-24 18:28:12 -0300

    Add files via upload

---
+++ gerflic_1-master/GerFlix 1.0/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Serie.h"
+#include "Usuario.h"
+#define TAMSERIE 20
+#define TAMUSUARIO 100
+
+
+
+#include <string.h>
+
+
+
+int main()
+{
+ int i,j;
+    eSerie listaDeSeries[TAMSERIE];
+    eUsuario listaDeUsuarios[TAMUSUARIO];
+    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
+
+    inicializarSeriesHardCode(listaDeSeries);
+    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
+    inicializarUsuariosHardCode(listaDeUsuarios);
+
+    /*
+
+    1. Mostrar el listado de series
+    2. Mostrar el listado de usuarios
+    3. Mostrar el listado de Usuarios con el nombre de la serie que ve
+    4. Mostrar por cada serie, el nombre de los usuarios que la ven.
+
+
+
+    */
+
+    for(i=0; i<5; i++){
+       printf("Serie: %s\n",listaDeSeries[i].nombre);
+    }
+
+    for(i=0; i<5; i++){
+       printf("usuario: %s\n",listaDeUsuarios[i].nombre);
+    }
+
+   for(i=0; i<5; i++)
+    {
+
+        printf("\n usuario: %s \n",listaDeUsuarios[i].nombre);
+
+        for(j=0; j<5; j++)
+        {
+            if (listaDeSeries[j].idSerie == listaDeUsuarios[i].idSerie)
+            {
+                printf("Serie: %s\n",listaDeSeries[i].nombre);
+            }
+        }
+
+
+    }
+
+    for(i=0; i<5; i++)
+    {
+
+        printf("\n Serie: %s \n",listaDeSeries[i].nombre);
+
+        for(j=0; j<5; j++)
+        {
+            if (listaDeUsuarios[j].idSerie == listaDeSeries[i].idSerie)
+            {
+                printf("usuario: %s\n",listaDeUsuarios[i].nombre);
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
